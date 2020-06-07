ffbe980a11c6af2a49dc48d6c2958a9a1c13b65a
Froylan <jmalaga@idom.com>
2020-06-07 13:21:33 +0200

    Reorganization 2

---
+++ os_specific/scripts/initial_packages.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Git
+sudo apt-get install git
+
+# Python3
+sudo apt-get install python3-venv # To create virtualenvs
+
+# Build Essential (needed to use 'make' command)
+sudo apt-get install build-essential # Need for 'make' command
+
+#Latex
+sudo apt-get install texlive-full
+
+# X11 package, needed by dwm
+#sudo apt install libx11-dev
+
