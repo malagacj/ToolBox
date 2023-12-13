1056cf5194a2f6e90bd63d251f4759cf538985db
malagacj <frilly_3@hotmail.com>
2023-12-13 22:27:15 +0100

    Update k8s_setup_03.sh

--- package_specific/Kubernetes/kubeadm_scripts/k8s_setup_03.sh
+++ package_specific/Kubernetes/kubeadm_scripts/k8s_setup_03.sh
@@ -1,55 +1,24 @@
 #! /bin/bash
+# https://kubernetes.io/docs/setup/production-environment/container-runtimes/#containerd
+## Install and configure prerequisites
 
 HOSTNAME=$(hostname)
 
-cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+# Forwarding IPv4 and letting iptables see bridged traffic
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 overlay
 br_netfilter
 EOF
+
 sudo modprobe overlay
 sudo modprobe br_netfilter
-cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-k8s.conf
-net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
 EOF
 
+# Apply sysctl params without reboot
 sudo sysctl --system
-
-
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
-
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg -y
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-sudo apt-get install containerd.io
-
-sudo rm /etc/containerd/config.toml
-sudo systemctl restart containerd
-sudo systemctl enable containerd
-
-sudo apt install gnupg gnupg2 curl software-properties-common -y
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/cgoogle.gpg
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-
-sudo apt update
-sudo apt install kubelet kubeadm kubectl -y
-sudo apt-mark hold kubelet kubeadm kubectl
-
-if [[ "$HOSTNAME" == *"master"* ]]; then
-  sudo kubeadm init --control-plane-endpoint=k8s-master
-  mkdir -p $HOME/.kube
-  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-  sudo chown $(id -u):$(id -g) $HOME/.kube/config
-fi
